c7af33ad516772fcdbdbf8c77283300bbc9900eb
priya <priyastar23897@gmail.com>
2018-07-18 17:13:10 +0530

    adding test file

---
+++ src/components/forms/test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+  render() {
+    return (
+      <div className="App">
+          LoginForm
+      </div>
+    );
+  }
+}
+
+export default Signup;
